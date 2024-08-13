bcc315e6757a3dfa9511cda616dce865ef9415d7
Ahmed-BS10 <ahmedbenshmalan@gmail.com>
2024-08-13 19:37:07 +0300

    add class and interfaces Account Services

---
+++ ECommerceApp.Business/Services/AccountServices.cs
@@ -0,0 +1,13 @@
+﻿using ECommerceApp.Business.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.Business.Services
+{
+    public class AccountServices : IAccountServices
+    {
+    }
+}
